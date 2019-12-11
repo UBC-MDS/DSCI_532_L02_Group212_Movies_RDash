f9cf465f327f657fa7bc817c08b43d7812ac73f2
Holly Williams <hwilliams@srk.com>
2019-12-10 17:58:41 -0800

    added auto filtering to comp plot

--- app.R
+++ app.R
@@ -88,6 +88,20 @@ chart_type <- dccRadioItems(
   )
 )
 
+# Select which movies to compare
+moviesDropdown <- dccDropdown(
+  id = "movies-dd",
+  # Credit to Kate S and teh participation app4.R example for the code below
+  # map/lapply can be used as a shortcut instead of writing the whole list
+  # especially useful if you wanted to filter by country!
+  options = map(
+    df_movies$title, function(x){
+      list(label=x, value=x)
+    }),
+  value = c("Alice in Wonderland", "Despicable Me", "Easy A"), #I picked these movies as default
+  multi = TRUE
+)
+
 
 ##############################################################################
 # JITTER / LINE GRAPH FUNCTION
@@ -243,6 +257,68 @@ make_graph_2 <- function(years=c(1980, 2010),
 #END BAR PLOT
 #######################################################################
 
+######################################################################
+# START COMPARISON PLOT
+######################################################################
+
+#bar plot
+make_graph_3 <- function(yaxis="worldwide_gross", inf="adj",
+                         movies_sel=c("Alice in Wonderland", "Despicable Me", "Easy A")){
+  
+  # gets the label matching the column value
+  if(inf=="adj"){
+    yaxisKey <- tibble(label = c("Box Office", "Profit", "Butts in Seats"),
+                       value = c("worldwide_adj", "worldwide_profit_adj", "worldwide_bits"))
+    title_end = " - Adjusted for inflation"
+  }
+  else{
+    yaxisKey <- tibble(label = c("Box Office", "Profit", "Butts in Seats"),
+                       value = c("worldwide_gross", "worldwide_profit_gross", "worldwide_bits"))
+    title_end = " - Not Adjusted for inflation"
+  }
+  
+  # if inflation selected, use adjusted values
+  if(inf=="adj"){
+    if(yaxis =="worldwide_gross"){
+      yaxis_to_plot = "worldwide_adj"
+    }
+    else if(yaxis == "worldwide_profit_gross"){
+      yaxis_to_plot = "worldwide_profit_adj"
+    }
+    else if(yaxis=="worldwide_bits"){
+      yaxis_to_plot = "worldwide_bits"
+    }
+  }
+  else yaxis_to_plot = yaxis
+  
+  y_label <- yaxisKey$label[yaxisKey$value==yaxis_to_plot]
+  
+  #filter our data based on the year/continent selections
+  data <- df_movies %>%
+    #filter(year >= years[1] & year <= years[2]) %>% 
+    filter(title %in% movies_sel) 
+  
+  p4 <- ggplot(data, aes(x=reorder(title, !!sym(yaxis_to_plot)), y=!!sym(yaxis_to_plot),  
+                         text = paste('</br> Movie: ', title,
+                                      '</br> Year: ', year,
+                                      '</br> Distributor: ', distributor,
+                                      '</br>', y_label,"(M): ", round(!!sym(yaxis_to_plot), 1)))) +
+    geom_bar(stat = 'identity', position="dodge") +
+    #scale_x_continuous(breaks = unique(data$year))+
+    scale_y_continuous(labels = comma) +
+    xlab("") +
+    ylab(paste0("Worldwide ", y_label, " (Millions)")) +
+    coord_flip() +
+    ggtitle(paste0("Comparing Selected Movies based on ", y_label, " (Millions)")) +
+    theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+          legend.position = "none")
+  ggplotly(p4, tooltip = "text") 
+}
+
+######################################################################
+# END COMPARISON PLOT
+######################################################################
 
 # Now we define the graph as a dash component using generated figure
 graph <- dccGraph(
@@ -254,6 +330,13 @@ graph2 <- dccGraph(
   id = 'bar-graph',
   figure=make_graph_2() # gets initial data using argument defaults
 )
+
+graph3 <- dccGraph(
+  id = 'comp-graph',
+  figure=make_graph_3() # gets initial data using argument defaults
+)
+
+
 
 ##########################################################################
 # START LAYOUT
@@ -275,11 +358,15 @@ app$layout(
       
       #graph 
       graph, 
-      htmlIframe(height=20, width=10, style=list(borderWidth = 0)), #space
+      htmlIframe(height=20, width=5, style=list(borderWidth = 0)), #space
       htmlLabel('Select a year range:'),
       yearSlider,
       htmlIframe(height=15, width=10, style=list(borderWidth = 0)), #space
       graph2,
+      htmlIframe(height=20, width=10, style=list(borderWidth = 0)), #space
+      htmlLabel('Select movies to compare:'),
+      moviesDropdown,
+      graph3,
       htmlIframe(height=20, width=10, style=list(borderWidth = 0)), #space
       dccMarkdown("[Data Source]()")
     )
@@ -299,7 +386,6 @@ app$layout(
 app$callback(
   #update figure of gap-graph
   output=list(id='gap-graph', property='figure'),
-  #based on values of year, continent, y-axis components
   params=list(input(id='year', property='value'),
               input(id='y-axis', property='value'),
               input(id='inf-type', property='value'),
@@ -311,15 +397,26 @@ app$callback(
 
 # Callback for bar chart
 app$callback(
-  #update figure of gap-graph
+  #update figure of bar-graph
   output=list(id='bar-graph', property='figure'),
-  #based on values of year, continent, y-axis components
   params=list(input(id='year', property='value'),
               input(id='y-axis', property='value'),
               input(id='inf-type', property='value')),
   #this translates your list of params into function arguments
   function(year_value, yaxis_value, inf_type) {
     make_graph_2(year_value, yaxis_value, inf_type)
+  })
+
+# Callback for comparison bar chart
+app$callback(
+  #update figure of comp-graph
+  output=list(id='comp-graph', property='figure'),
+  params=list(input(id='y-axis', property='value'),
+              input(id='inf-type', property='value'),
+              input(id='movies-dd', property='value')),
+  #this translates your list of params into function arguments
+  function(yaxis_value, inf_type, movies_dd) {
+    make_graph_3(yaxis_value, inf_type, movies_dd)
   })
 
 #####################################################################
